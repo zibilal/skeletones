bb2ca9b51fbb5d30737c373841fdeda9a25f2190
Bilal Muhammad(jibilal) <bilal.muhammad@kudo.co.id>
2018-05-08 11:13:47 +0700

    Add commit id on OrderDTO

--- ui/apitest/handler.go
+++ ui/apitest/handler.go
@@ -3,10 +3,11 @@ package apitest
 import (
 	"net/http"
 	"github.com/julienschmidt/httprouter"
+	"fmt"
 )
 
 func ListOrders(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
+	fmt.Println("List of orders")
 }
 
 func CreateOrder(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
